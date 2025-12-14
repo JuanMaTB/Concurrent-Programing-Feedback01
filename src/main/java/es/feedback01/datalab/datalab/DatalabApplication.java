c2d813e4c9d4d544619cf1101c6ea398a7a90eb6
Juan Manuel Torrado <jm.torrado.borrachero@gmail.com>
2025-12-14 14:36:39 +0100

    optimizacion de git

---
+++ src/main/java/es/feedback01/datalab/datalab/DatalabApplication.java
@@ -0,0 +1,13 @@
+package es.feedback01.datalab.datalab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatalabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatalabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/feedback01/datalab/datalab/DatalabApplicationTests.java
@@ -0,0 +1,13 @@
+package es.feedback01.datalab.datalab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatalabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
